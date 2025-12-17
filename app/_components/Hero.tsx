b85e278499df07ec76ec836952fd8fadb215d6f9
mdjameel04 <jameeltony67@gmail.com>
2025-12-17 12:39:54 +0530

    inital commit

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -10,21 +10,24 @@ const Hero = () => {
 <Spotlight className='-top-10 left-full h-[80vh] w-[50vw] ' fill='purple'/>
 <Spotlight className='-top-28 left-80 h-[80vh] w-[50vw] ' fill='blue'/>
     </div>
- <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-black">
+ <div className="absolute top-0 left-0 flex h-screen w-full items-center justify-center bg-white dark:bg-black-150">
       <div
         className={cn( "absolute inset-0",
-          "[background-size:80px_80px]",
           "[background-image:linear-gradient(to_right,#a1a1a1_1px,transparent_3px),linear-gradient(to_bottom,#e4e4e7_3px,transparent_1px)]",
-          "dark:[background-image:linear-gradient(to_right,#171717_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#e5e7eb_1px,transparent_1px),linear-gradient(to_bottom,#f4f4f4_1px,transparent_1px)]",
         )}
       />
-      {/* Radial gradient for the container to give a faded look */}
-      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
-      <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
-        
-      </p>
     </div>
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
 
+   <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </p>
+
+    </div>
+    </div>
     </div>
   )
 }
