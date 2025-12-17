cd58ebf2b93aa95c35a75a355e629ff0a916e815
mdjameel04 <jameeltony67@gmail.com>
2025-12-18 00:21:13 +0530

    first commit

---
+++ Data/index.ts
@@ -0,0 +1,6 @@
+export const gridItems =[
+    { title: "title", description: "desc1", id: 1},
+    { title: "title", description: "desc1", id: 2},
+    { title: "title", description: "desc1", id: 3},
+    { title: "title", description: "desc1", id: 4},
+]
--- app/_components/Grid.tsx
+++ app/_components/Grid.tsx
@@ -1,11 +1,12 @@
 import { BentoGrid, BentoGridItem } from '@/components/ui/bento-grid'
+import { gridItems } from '@/Data'
 import React from 'react'
 
 const Grid = () => {
   return (
     <section id='about'>
 <BentoGrid>
-    {[{ title: "title", description: "desc1", id: 1}].map((item)=>(
+   {gridItems.map((item)=>(
         <BentoGridItem
             id={item.id} key={item.id} 
             title={item.title} description={item.description} >
